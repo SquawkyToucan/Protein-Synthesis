3b022e9d926e2f79c5db8f7adc90f7c94f3299fc
SquawkyToucan <ewbrowne743@gmail.com>
2018-03-08 12:38:26 -0800

    Finished codons that start with A

--- Protein-Synthesis/DNA Decoder/src/Decode.java
+++ Protein-Synthesis/DNA Decoder/src/Decode.java
@@ -48,13 +48,42 @@ public class Decode {
 					output.setText(output.getText() + "Lysine ");
 					
 				}
+				else if (t.substring(2, 3).equalsIgnoreCase("C") || t.substring(2, 3).equalsIgnoreCase("T")){
+					// AAT, AAC
+					output.setText(output.getText() + "Asparagine ");
+				}
 			}
-		} else if (t.substring(0, 1).equalsIgnoreCase("T")) {
-			// Begins with T
+			else if(t.substring(1, 2).equalsIgnoreCase("G")) {
+				// AG
+				if (t.substring(2, 3).equalsIgnoreCase("G") || t.substring(2, 3).equalsIgnoreCase("A")) {
+					// ATG, ATA
+					output.setText(output.getText() + "Arginine ");
+					
+				}
+				else if (t.substring(2, 3).equalsIgnoreCase("C") || t.substring(2, 3).equalsIgnoreCase("T")){
+					// ATT, ATC
+					output.setText(output.getText() + "Serine ");
+				}
+			}
+			else if(t.substring(1, 2).equalsIgnoreCase("C")) {
+				// AC
+				output.setText(output.getText() + "Threorine ");
+			}
+			else if(t.substring(1, 2).equalsIgnoreCase("T")) {
+				// AT
+				if(t.substring(2, 3).equalsIgnoreCase("G")) {
+					output.setText(output.getText() +  "Methionine "); 
+				}
+				else if(t.substring(2, 3).equalsIgnoreCase("T")||t.substring(2, 3).equalsIgnoreCase("A")||t.substring(2, 3).equalsIgnoreCase("C")) {
+					output.setText(output.getText() + "Isoleucine ");
+				}
+			}
 		} else if (t.substring(0, 1).equalsIgnoreCase("G")) {
 			// Begins with G
 		} else if (t.substring(0, 1).equalsIgnoreCase("C")) {
 			// Begins with C
+		} else if (t.substring(0, 1).equalsIgnoreCase("T")) {
+			// Begins with T
 		} 
 		in.setText("");
 	}
