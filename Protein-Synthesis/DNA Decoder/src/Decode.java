f39110a1b9b44c998f397def28ed19c1a6e84826
SquawkyToucan <ewbrowne743@gmail.com>
2018-03-09 15:23:34 -0800

    Finished

--- Protein-Synthesis/DNA Decoder/src/Decode.java
+++ Protein-Synthesis/DNA Decoder/src/Decode.java
@@ -12,7 +12,7 @@ public class Decode {
 	JPanel panel = new JPanel();
 	JTextField in = new JTextField(20);
 	JTextArea output = new JTextArea();
-
+	boolean notStopped = true;
 	Decode() {
 		in.setText("");
 		output.setWrapStyleWord(true);
@@ -28,7 +28,7 @@ public class Decode {
 		JOptionPane.showMessageDialog(null,
 				"This will be translating directly off of DNA, which uses the bases A, T, C, and G.");
 		while (true) {
-			if (in.getText() != null && in.getText() != "") {
+			if (in.getText() != null && in.getText() != "" && notStopped) {
 				if (in.getText().length() == 3) {
 					translate();
 				}
@@ -99,8 +99,57 @@ public class Decode {
 			}
 		} else if (t.substring(0, 1).equalsIgnoreCase("C")) {
 			// Begins with C
+			if(t.substring(1,2).equalsIgnoreCase("A")) {
+				if(t.substring(2,3).equalsIgnoreCase("A") || t.substring(2,3).equalsIgnoreCase("G")) {
+					output.setText(output.getText() + "Glutamine ");
+				}
+				else if(t.substring(2,3).equalsIgnoreCase("T") || t.substring(2,3).equalsIgnoreCase("C")) {
+					output.setText(output.getText() + "Histidine ");
+				}
+			}
+			else if(t.substring(1,2).equalsIgnoreCase("C")) {
+				output.setText(output.getText() + "Proline ");
+			}
+			else if(t.substring(1,2).equalsIgnoreCase("T")) {
+				output.setText(output.getText() + "Leucine ");
+			}
+			else if(t.substring(1,2).equalsIgnoreCase("G")) {
+				output.setText(output.getText() + "Arginine ");
+			}
 		} else if (t.substring(0, 1).equalsIgnoreCase("T")) {
 			// Begins with T
+			if(t.substring(1,2).equalsIgnoreCase("A")) {
+				if(t.substring(2,3).equalsIgnoreCase("A") || t.substring(2,3).equalsIgnoreCase("G")) {
+					output.setText(output.getText() + "Stop Codon");
+					notStopped = false;
+				}
+				else if(t.substring(2,3).equalsIgnoreCase("T") || t.substring(2,3).equalsIgnoreCase("C")) {
+					output.setText(output.getText() + "Tyrosine ");
+				}
+			}
+			else if(t.substring(1,2).equalsIgnoreCase("C")) {
+				output.setText(output.getText() + "Serine ");
+			}
+			else if(t.substring(1,2).equalsIgnoreCase("G")) {
+				if(t.substring(2,3).equalsIgnoreCase("A")) {
+					output.setText(output.getText() + "Stop Codon");
+					notStopped = false;
+				}
+				else if(t.substring(2,3).equalsIgnoreCase("G")) {
+					output.setText(output.getText() + "Tryptophan ");
+				}
+				else if(t.substring(2,3).equalsIgnoreCase("T") || t.substring(2,3).equalsIgnoreCase("C")) {
+					output.setText(output.getText() + "Tyrosine ");
+				}
+			}
+			else if(t.substring(1,2).equalsIgnoreCase("T")) {
+				if(t.substring(2,3).equalsIgnoreCase("A") || t.substring(2,3).equalsIgnoreCase("G")) {
+					output.setText(output.getText() + "Leucine");
+				}
+				else if(t.substring(2,3).equalsIgnoreCase("T") || t.substring(2,3).equalsIgnoreCase("C")) {
+					output.setText(output.getText() + "Phenylalanine ");
+				}
+			}
 		} 
 		in.setText("");
 	}
